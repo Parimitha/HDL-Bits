5d324645c5ca2449f81d61989e3d1ab35ab3e787
Parimitha <119649002+Parimitha@users.noreply.github.com>
2025-06-03 22:49:22 +0530

    andgate_tb.v

---
+++ andgate_tb.v
@@ -0,0 +1,11 @@
+module top_module();
+    reg [1:0]in;
+    wire out;
+    andgate uut(.in(in),.out(out));
+    initial begin
+        in=00;
+        #10 in=01;
+        #10 in=10;
+        #10 in=11;
+    end
+endmodule
