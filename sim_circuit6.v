e978851323fef70d95b3af3f00808a73c7b496e4
Parimitha <119649002+Parimitha@users.noreply.github.com>
2025-06-09 12:12:18 +0530

    sim_circuit6.v

---
+++ sim_circuit6.v
@@ -0,0 +1,18 @@
+module top_module (
+    input [2:0] a,
+    output [15:0] q ); 
+    always@(*)
+        begin
+            case(a)
+                4'd0: q<=16'h1232;
+                4'd1: q<=16'haee0;
+                4'd2: q<=16'h27d4;
+                4'd3: q<=16'h5a0e;
+                4'd4: q<=16'h2066;
+                4'd5: q<=16'h64ce;
+                4'd6: q<=16'hc526;
+                4'd7: q<=16'h2f19;
+                default: q<=16'hf;
+            endcase
+        end
+endmodule
