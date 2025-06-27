84a1a2d69a7c03f09c09ca2b4d8e0cb84d5e68aa
Parimitha <119649002+Parimitha@users.noreply.github.com>
2025-06-27 11:22:35 +0530

    fsmq6b.v

---
+++ fsmq6b.v
@@ -0,0 +1,21 @@
+module top_module (
+    input [3:1] y,
+    input w,
+    output Y2);
+    parameter A=3'b000,B=3'b001,C=3'b010,D=3'b011,E=3'b100,F=3'b101;
+    reg [2:0]ns;
+    always@(*)
+        begin
+            case(y)
+                A: ns=(w)?A:B;
+                B: ns=(w)?D:C;
+                C: ns=(w)?D:E;
+                D: ns=(w)?A:F;
+                E: ns=(~w)?E:D;
+                F: ns=(~w)?D:C;
+                default: ns=3'b000;
+            endcase
+        end
+    assign Y2=ns[1];
+                
+endmodule
